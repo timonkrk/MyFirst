e142dfbac3bf9d246f620826cf64507b91a7bce1
Michal <Michal@MJ>
2021-09-27 18:13:11 +0200

    Dodaj pliki projektów.

---
+++ MyFirst/MyFirst.cpp
@@ -0,0 +1,12 @@
+﻿//myfirst.cpp
+
+#include <iostream>
+int main()
+{
+	using namespace std;
+	cout << "Let's play C++ language.";
+	cout << endl;
+	cout << "You'll not regret!" << endl;
+	cin.get();
+	return 0;
+}
